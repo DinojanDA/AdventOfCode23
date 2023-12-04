1d38458b0b23522447badc9cabe1f194c07fac73
Dinojan <dinojan.david-anton9@etu.univ-lorraine.fr>
2023-12-04 19:06:40 +0100

    Résolution de la partie 2 du day04

--- day04/day04.go
+++ day04/day04.go
@@ -33,6 +33,16 @@ func points(cards []string) int {
 	return int(math.Pow(2, l-1))
 }
 
+func sumTab(tab []int, len int) int {
+	var sum int = 0
+	var i int = 0
+	for i < len {
+		sum += tab[i]
+		i++
+	}
+	return sum
+}
+
 func part1(str string) int {
 	var sum int = 0
 	var games []string = strings.Split(str, "\n")
@@ -49,10 +59,30 @@ func part1(str string) int {
 }
 
 func part2(str string) int {
-	return 0
+	var games []string = strings.Split(str, "\n")
+	var copyTables []int = make([]int, 2*len(games))
+	for i, game := range games {
+		copyTables[i] += 1
+		var cards []string = strings.Split(game, ": ")
+		cards[1] = strings.ReplaceAll(cards[1], "  ", " ")
+		var hands []string = strings.Split(cards[1], " | ")
+		var winningCards []string = strings.Split(betterString(hands[0]), " ")
+		var myCards []string = strings.Split(betterString(hands[1]), " ")
+		var point int = len(search(winningCards, myCards))
+		var j int = 0
+		for j < point {
+			copyTables[i+1+j] += copyTables[i]
+			j++
+		}
+	}
+	var sum int = sumTab(copyTables, len(games))
+	return sum
 }
 func main() {
 	var sol1 int = part1(inputday)
 	fmt.Println(sol1)
+
+	var sol2 int = part2(inputday)
+	fmt.Println(sol2)
 
 }
--- day04/day04_test.go
+++ day04/day04_test.go
@@ -8,16 +8,29 @@ import (
 //go:embed input_test.txt
 var inputtest string
 
-/*var games[]string = strings.Split(inputtest, "\n")
-var cards []string = strings.Split(games, ": ")
-var hands []string = strings.Split(cards[1], " | ")
-var winningCards []string = strings.Split(hands[0], " ")
-var myCards []string = strings.Split(hands[1], " ")*/
+func TestSearch(t *testing.T) {
+	var winningCards []string = []string{"2", "3", "4"}
+	var myCards []string = []string{"4", "1", "8", "3", "7"}
+	var expected []string = []string{"4", "3"}
+	sols := search(winningCards, myCards)
+	for i, sol := range sols {
+		if sol != expected[i] {
+			t.Errorf("Expected %s, got %s", expected[i], sol)
+		}
+	}
+}
 
 func TestPart1(t *testing.T) {
 	var sol1 int = part1(inputtest)
 	var expected int = 13
 	if sol1 != expected {
 		t.Errorf("Expected %d, got %d", expected, sol1)
+	}
+}
+func TestPart2(t *testing.T) {
+	var sol2 int = part2(inputtest)
+	var expected int = 30
+	if sol2 != expected {
+		t.Errorf("Expected %d, got %d", expected, sol2)
 	}
 }

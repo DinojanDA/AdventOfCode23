215108c4f0866fb609e505525534b299fa1296e3
Dinojan <dinojan.david-anton9@etu.univ-lorraine.fr>
2023-12-06 15:53:57 +0100

    Résolution de la partie 1 et 2 du day06

---
+++ day06/day06.go
@@ -0,0 +1,100 @@
+package main
+
+import (
+	_ "embed"
+	"fmt"
+	"strconv"
+	"strings"
+)
+
+//go:embed input.txt
+var inputday string
+
+func findMax(numbers []int) int {
+	if len(numbers) == 0 {
+		return -1
+	}
+	maxi := numbers[0]
+	for _, num := range numbers {
+		if num > maxi {
+			maxi = num
+		}
+	}
+	return maxi
+}
+func convertToTabInt(str string) []int {
+	numbersStr := strings.Fields(str)
+	var numbers []int
+	for _, numStr := range numbersStr {
+		num, err := strconv.Atoi(numStr)
+		if err == nil {
+			numbers = append(numbers, num)
+		}
+	}
+	return numbers
+}
+
+func arrive(hold int, time int, distance int) bool {
+	if hold > time {
+		return false
+	}
+	return hold*(time-hold) > distance
+}
+
+func multTab(tab []int) int {
+	var len int = len(tab)
+	var sum int = 1
+	var i int = 0
+	for i < len {
+		sum *= tab[i]
+		i++
+	}
+	return sum
+}
+
+func part1(str string) int {
+	var categories []string = strings.Split(inputday, "\r\n")
+
+	var strTimes string = strings.Split(categories[0], ":")[1]
+	var strDistances string = strings.Split(categories[1], ":")[1]
+	var times []int = convertToTabInt(strTimes)
+	var distances []int = convertToTabInt(strDistances)
+	var winWays []int = make([]int, len(times))
+	maxTime := findMax(times)
+
+	for i := 0; i < maxTime+1; i++ {
+		for j := 0; j < len(times); j++ {
+			if arrive(i, times[j], distances[j]) {
+				winWays[j] += 1
+			}
+		}
+	}
+	return multTab(winWays)
+}
+
+func part2(str string) int {
+	var categories []string = strings.Split(inputday, "\r\n")
+
+	var strTimes string = strings.Split(categories[0], ":")[1]
+	var strDistances string = strings.Split(categories[1], ":")[1]
+	var strTime string = strings.ReplaceAll(strTimes, " ", "")
+	var strDistance string = strings.ReplaceAll(strDistances, " ", "")
+	time, _ := strconv.Atoi(strTime)
+	distance, _ := strconv.Atoi(strDistance)
+	var winWays int = 0
+
+	for i := 0; i < time; i++ {
+		if arrive(i, time, distance) {
+			winWays += 1
+		}
+	}
+	return winWays
+}
+
+func main() {
+	var sol1 int = part1(inputday)
+	fmt.Println(sol1)
+
+	var sol2 int = part2(inputday)
+	fmt.Println(sol2)
+}
---
+++ day06/day06_test.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	_ "embed"
+	"testing"
+)
+
+//go:embed input_test.txt
+var inputtest string
+
+func TestOneRace(t *testing.T) {
+	var time int = 7
+	var distance int = 9
+	var count int = 0
+	for i := 0; i < 8; i++ {
+		if arrive(i, time, distance) {
+			count += 1
+		}
+	}
+	var sol int = count
+	var expected int = 4
+	if sol != expected {
+		t.Errorf("Expected %d, got %d", expected, sol)
+	}
+}
+
+func TestPart1(t *testing.T) {
+	var sol1 int = part1(inputtest)
+	var expected int = 288
+	if sol1 != expected {
+		t.Errorf("Expected %d, got %d", expected, sol1)
+	}
+}
+
+func TestPart2(t *testing.T) {
+	var sol2 int = part2(inputtest)
+	var expected int = 71503
+	if sol2 != expected {
+		t.Errorf("Expected %d, got %d", expected, sol2)
+	}
+}

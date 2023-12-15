95fd996a469498cd3ae2f03466a92b4f9cfc3484
Dinojan <dinojan.david-anton9@etu.univ-lorraine.fr>
2023-12-15 20:16:00 +0100

    Résolution de la partie 2 du day15

--- day15/day15.go
+++ day15/day15.go
@@ -3,6 +3,7 @@ package main
 import (
 	_ "embed"
 	"fmt"
+	"strconv"
 	"strings"
 )
 
@@ -18,6 +19,59 @@ func calculate(word string) int {
 	}
 	return c
 }
+
+func parseInstruction(instruction string) (string, string, int) {
+	var label string
+	var operation string
+	var value int
+	if strings.Contains(instruction, "=") {
+		parts := strings.Split(instruction, "=")
+		label = parts[0]
+		operation = "="
+		value, _ = strconv.Atoi(parts[1])
+	} else {
+		label = strings.TrimSuffix(instruction, "-")
+		operation = "-"
+		value = -1
+	}
+	return label, operation, value
+}
+
+func isPresent(matrix [256][]string, st string) int {
+	var i int = calculate(st)
+	var j int = 0
+	for j < len(matrix[i]) {
+		s := strings.Split(matrix[i][j], " ")
+		if s[0] == st {
+			return j
+		}
+		j++
+	}
+	return -1
+}
+
+func remove(matrix *[256][]string, st string) {
+	i := calculate(st)
+	j := isPresent(*matrix, st)
+	if j != -1 {
+		matrix[i] = append(matrix[i][:j], matrix[i][j+1:]...)
+	}
+}
+
+func calculateMatrix(matrix [256][]string) int {
+	var sum int
+	for i, row := range matrix {
+		for j, cell := range row {
+			if cell != "" {
+				parts := strings.Split(cell, " ")
+				value, _ := strconv.Atoi(parts[1])
+				sum += (i + 1) * (j + 1) * value
+			}
+		}
+	}
+	return sum
+}
+
 func part1(str string) int {
 	var words []string = strings.Split(str, ",")
 	var sum int = 0
@@ -28,7 +82,28 @@ func part1(str string) int {
 }
 
 func part2(str string) int {
-	return 0
+	var boxes [256][]string
+	var words []string = strings.Split(str, ",")
+	for _, word := range words {
+		st, op, a := parseInstruction(word)
+		c := calculate(st)
+		if op == "=" {
+			var l int = isPresent(boxes, st)
+			if l != -1 {
+				s := strconv.Itoa(a)
+				boxes[c][l] = st + " " + s
+			} else {
+				s := strconv.Itoa(a)
+				boxes[c] = append(boxes[c], st+" "+s)
+			}
+		} else {
+			var l int = isPresent(boxes, st)
+			if l != -1 {
+				remove(&boxes, st)
+			}
+		}
+	}
+	return calculateMatrix(boxes)
 }
 func main() {
 	var sol1 int = part1(inputday)
